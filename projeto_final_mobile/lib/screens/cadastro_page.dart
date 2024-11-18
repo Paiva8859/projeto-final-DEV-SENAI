ddb50198a4c95d29739d02c3f757c628bbb2770d
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2024-11-18 15:02:31 -0300

    ok

--- projeto_final_mobile/lib/screens/cadastro_page.dart
+++ projeto_final_mobile/lib/screens/cadastro_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class CadastroPage extends StatelessWidget {
