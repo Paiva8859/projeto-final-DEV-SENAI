85c744789d733913316d230c4ca392900402fa67
DaviEspinosa <davipedrosa037@gmail.com>
2024-11-26 16:03:42 -0300

    ok

--- voluntaria-limeira/src/app/service/cadastroRecompensa.js
+++ voluntaria-limeira/src/app/service/cadastroRecompensa.js
@@ -9,26 +9,10 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 
-
 async function cadastroRecompensa(titulo, descricao, inicio, termino) {
   try {
-    // Construa as datas manualmente para evitar erros
-    const [diaInicio, mesInicio, anoInicio] = inicio.split("/").map(Number);
-    const [diaTermino, mesTermino, anoTermino] = termino.split("/").map(Number);
-
-    // Cria a data no formato ano, mês (base 0), dia
-    const dataInicio = new Date(anoInicio, mesInicio - 1, diaInicio);
-    const dataExpiracao = new Date(anoTermino, mesTermino - 1, diaTermino);
-
-    // Formata as datas para o formato "dd/MM/yyyy"
-    const dataInicioFormatada = `${Number(diaInicio).padStart(2, "0")}/${Number(
-      mesInicio
-    ).padStart(2, "0")}/${anoInicio}`;
-    const dataExpiracaoFormatada = `${Number(diaTermino).padStart(
-      2,
-      "0"
-    )}/${String(mesTermino).padStart(2, "0")}/${anoTermino}`;
-
+    const dataInicio = new Date(inicio);
+    const dataExpiracao = new Date(termino);
 
     // Somando 1 dia (24 horas) à data
     dataInicio.setTime(dataInicio.getTime() + 24 * 60 * 60 * 1000);
@@ -37,24 +21,19 @@ async function cadastroRecompensa(titulo, descricao, inicio, termino) {
     await setDoc(doc(db, "Recompensas", titulo), {
       titulo: titulo,
       descricao: descricao,
-      dataInicio: Number(dataInicioFormatada),
-      dataExpiracao: dataExpiracaoFormatada,
+      dataInicio: Timestamp.fromDate(dataInicio),
+      dataExpiracao: Timestamp.fromDate(dataExpiracao),
       verificado: false,
     });
 
     console.log(`Recompensa criada com sucesso: ${titulo}`);
-    return {
-      titulo,
-      descricao,
-      dataInicio: dataInicioFormatada,
-      dataExpiracao: dataExpiracaoFormatada,
-    };
+    return { titulo, descricao, dataInicio, dataExpiracao };
+
   } catch (err) {
     console.error("Erro ao criar recompensa: ", err);
     throw err;
   }
 }
-
 
 
 async function verificarRecompensasExpiradas() {
