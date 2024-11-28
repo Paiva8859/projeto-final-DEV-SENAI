3fd58934f9f94abb27820e4415da14203c772710
DaviEspinosa <davipedrosa037@gmail.com>
2024-11-28 15:04:49 -0300

    ok

--- voluntaria-limeira/src/app/components/recompensa.js
+++ voluntaria-limeira/src/app/components/recompensa.js
@@ -39,6 +39,7 @@ function Recompensa() {
     if (estiloForm.display === "none") {
       formulario.style.display = "flex";
       botao.textContent = "x";
+      
     } else {
       formulario.style.display = "none";
       botao.textContent = "+";
@@ -49,63 +50,72 @@ function Recompensa() {
   return (
     <>
       <div className={style.sectionRecompensa}>
-        <h2>Cadastre uma Recompensa</h2>
         <form id="formRecompensa" className={style.form}>
-          <div>
-            <label htmlFor="tituloRecompensa">Título da Recompensa</label>
-            <input
-              type="text"
-              id="tituloRecompensa"
-              value={titulo}
-              onChange={(e) => setTitulo(e.target.value)}
-              required
-            />
-          </div>
+          <h2 style={{ margin: 0 }}>Cadastre Recompensa</h2>
+          <div className={style.infoForm}>
+            <div className={style.containerInput}>
+              {/* <label htmlFor="tituloRecompensa">Título da Recompensa</label> */}
+              <input
+              style={{color:'#797979', outline:'0'}}
+                type="text"
+                id="tituloRecompensa"
+                placeholder="Título Recompensa"
+                value={titulo}
+                onChange={(e) => setTitulo(e.target.value)}
+                required
+              />
+            </div>
 
-          <div>
-            <label htmlFor="descricaoRecompensa">Descrição da Recompensa</label>
-            <textarea
-              id="descricaoRecompensa"
-              value={descricao}
-              onChange={(e) => setDescricao(e.target.value)}
-              required
-            />
-          </div>
+            <div className={style.containerInput}>
+              {/* <label htmlFor="descricaoRecompensa">Descrição da Recompensa</label> */}
+              <textarea
+              style={{border: '0', borderBottom: '1.5px solid orange',color:'#797979', outline:'0', fontFamily:'Gill Sans MT' }}
+                id="descricaoRecompensa"
+                placeholder="Descrição Recompensa"
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)}
+                required
+              />
+            </div>
 
-          <div>
-            <label htmlFor="dataInicio">Data Início</label>
-            <input
-              type="date"
-              placeholder="Início"
-              value={inicio}
-              onChange={(e) => { setInicio(e.target.value, 0) }}
-            // requires
-            />
-          </div>
-          <div>
-            <label htmlFor="dataFinal">Data Final</label>
-            <input
-              type="date"
-              placeholder="Final"
-              value={termino}
-              onChange={(e) => { setTermino(e.target.value, 0) }}
-            // required
-            />
-          </div>
-          <div>
-            <label htmlFor="quantidade">Quantidade</label>
-            <input
-              type="number"
-              placeholder="Quantidade"
-              value={quantidade}
-              onChange={(e) => { setQuantidade(e.target.value, 0) }}
-            // required
-            />
-          </div>
+            <div className={style.containerInput}>
+              {/* <label htmlFor="dataInicio">Data Início</label> */}
+              <input
+              style={{color:'#797979', outline:'0'}}
+                type="date"
+                // placeholder="Início"
+                value={inicio}
+                onChange={(e) => { setInicio(e.target.value, 0) }}
+              // requires
+              />
+            </div>
+            <div className={style.containerInput}>
+              {/* <label htmlFor="dataFinal">Data Final</label> */}
+              <input
+              style={{color:'#797979', outline:'0'}}
+                type="date"
+                // placeholder="Final"
+                value={termino}
+                onChange={(e) => { setTermino(e.target.value, 0) }}
+              // required
+              />
+            </div>
+            <div className={style.containerInput}>
+              {/* <label htmlFor="quantidade">Quantidade</label> */}
+              <input
+              style={{color:'#797979', outline:'0'}}
+                type="number"
+                placeholder="Quantidade"
+                value={quantidade}
+                onChange={(e) => { setQuantidade(e.target.value, 0) }}
+              // required
+              />
+            </div>
 
-          {mensagem && <p className={style.mensagem}>{mensagem}</p>}
+            {mensagem && <p className={style.mensagem}>{mensagem}</p>}
 
-          <button className={style.btnCadastroRecompensa} onClick={criarNova}>Cadastrar</button>
+            <button className={style.btnCadastroRecompensa} onClick={criarNova}>Cadastrar</button>
+          </div>
         </form>
       </div>
 
