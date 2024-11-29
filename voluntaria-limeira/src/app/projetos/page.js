ef6c310b177c94f4de8285de152f47ed15d7bf9e
DaviEspinosa <davipedrosa037@gmail.com>
2024-11-29 16:22:55 -0300

    a

--- voluntaria-limeira/src/app/projetos/page.js
+++ voluntaria-limeira/src/app/projetos/page.js
@@ -4,8 +4,6 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import style from "@/app/style/projetos.module.css";
 import { useAuth } from "../service/authContext";
-import CadastroProjeto from "../components/cadastroRecompensa";
-import AceitarProjetos from "../components/aceitarProjetos";
 import ListarProjetos from "../components/listarProjetos";
 
 function Projetos() {
